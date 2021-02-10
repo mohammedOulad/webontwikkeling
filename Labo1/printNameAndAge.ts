021fa372a774486f8100ed8c1dd54f92513fe271
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-02-10 14:46:58 +0100

    Labo 1 gedaan

---
+++ Labo1/printNameAndAge.ts
@@ -0,0 +1,5 @@
+let voornaam = "mohammed" ;
+let achternaam = "oulad mhand";
+let leeftijd = 21;
+let uitkomst = `Ik ben ${voornaam} ${achternaam} en ik ben ${leeftijd} jaar oud`;
+console.log(uitkomst);
