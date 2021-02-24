fa32ada9229ea2ffdbd2fddda0d2168de72dea44
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-02-24 14:54:09 +0100

    verbetering oefening3 veelvouden 5 en 3

--- Labo2/oefening3.ts
+++ Labo2/oefening3.ts
@@ -11,7 +11,7 @@ for (let i = 0; i < 70; i++)
 for (let i = 0; i < 70; i++)
 {   
         veelvouden[i] = i+1;
-        if ((veelvouden[i] % 3 === 0 || veelvouden[i] % 5 === 0))
+        if ((veelvouden[i] % 3 === 0 || veelvouden[i] % 5 === 0) && !(veelvouden[i] % 3 === 0 || veelvouden[i] % 5 === 0))
     {
         console.log(veelvouden[i]);
     }
