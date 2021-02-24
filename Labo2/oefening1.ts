95af9bcbac5f5b50727142b2d73e542cba368e8e
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-02-24 13:47:34 +0100

    modulo toevoegen

--- Labo2/oefening1.ts
+++ Labo2/oefening1.ts
@@ -1,10 +1,15 @@
 let getallen = new Array(100);
-let uitprinten = getallen.length;
 for (let i = 99; i < 200; i++)
     {  
         getallen[i] = i + 1; 
-}
+    }
 while(getallen.length <= 200)
-    {console.log(getallen);
-    break;
-}
+    {
+        console.log(getallen);
+        break;
+    }
+    for (let i = 99; i < 200; i++)
+    {  
+        if(getallen[i] % 2 ==0)
+        getallen[i] = i + 1; 
+    }
