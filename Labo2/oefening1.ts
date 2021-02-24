7f7f957edbce18e176d827ceb721b9b01b740a24
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-02-24 13:24:26 +0100

    strugle met while

--- Labo2/oefening1.ts
+++ Labo2/oefening1.ts
@@ -1,4 +1,10 @@
-var getallen = new Array(100);
-for (var i = 100; i < 200; i++)
- { getallen[i] = i + 1; 
-console.log(getallen[i]); }
+let getallen = new Array(100);
+while ()
+{
+for (let i = 99; i < 200; i++)
+    {  
+        getallen[i] = i + 1; 
+        console.log(getallen[i]); 
+    }
+
+}
