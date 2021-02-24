546362bdbb926aab614e204cb389cf4e954b7440
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-02-24 15:36:04 +0100

    oefening 2 gedaan

--- Labo2/oefening2.ts
+++ Labo2/oefening2.ts
@@ -5,6 +5,10 @@ Red, Orange, Yellow, Green, Blue, Indigo, Violet
 let legearray = new Array(kleur);
 for (let index = 0; index < legearray.length; index++)
 {
-    console.log(legearray[index])
+    legearray[index]
 }
-
+while(legearray.length)
+{
+    console.log(legearray);
+    break;
+}
