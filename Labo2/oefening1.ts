11fd4aa71e977abc958f9e353146142dd295fb81
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-02-24 14:43:45 +0100

    oefening1 correct gedaan

--- Labo2/oefening1.ts
+++ Labo2/oefening1.ts
@@ -4,7 +4,6 @@ for (let i = 0; i < 100; i++)
     {  
         let teller = 100 +(i+1);
         getallen[i] = teller; 
-        
     }
     while(getallen.length <= 100)
     {
@@ -13,6 +12,8 @@ for (let i = 0; i < 100; i++)
     }
 for (let i = 0; i < 100; i++)
 {
+    let teller1 = 100 + (i+1);
+    getallen[i] = teller1;
     if (getallen[i] % 3 === 0)
     {
     console.log(getallen[i]);
---
+++ Labo2/oefening3.ts
@@ -0,0 +1,11 @@
+let veelvouden = new Array();
+
+for (let i = 0; i < 70; i++)
+    {  
+        if (getallen[i] % 7 === 0)
+    {
+        console.log(getallen[i]);
+        let teller = 70 +(i+1);
+        veelvouden[i] = teller;  
+    }
+    }
