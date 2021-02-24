0da711bef2d5dd57c9b4400de3e8c75b08981110
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-02-24 16:20:10 +0100

    reverse array toevoegen

--- Labo2/oefening2.ts
+++ Labo2/oefening2.ts
@@ -3,9 +3,19 @@ enum kleur
 Red, Orange, Yellow, Green, Blue, Indigo, Violet
 }
 let legearray = new Array(kleur);
-for (let index = 0; index < legearray.length; index++)
+/*for (let index = 0; index < legearray.length; index++)
 {
-    legearray[index]
+    legearray[index] 
+}
+while(legearray.length <= 6)
+{
+    console.log(legearray);
+    break;
+}*/
+
+for (let index = 6; index > legearray.length ; index--)
+{
+    legearray[index] 
 }
 while(legearray.length)
 {
