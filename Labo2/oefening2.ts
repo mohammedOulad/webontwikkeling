479080a629a9e6b9d28971d7079bc350d1845e30
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-02-24 15:33:06 +0100

    waarde/kleuren toevoegen

--- Labo2/oefening2.ts
+++ Labo2/oefening2.ts
@@ -1,3 +1,10 @@
-enum kleur{
-    Red, Orange, Yellow, Green, Blue, Indigo, Violet
+enum kleur
+{
+Red, Orange, Yellow, Green, Blue, Indigo, Violet
 }
+let legearray = new Array(kleur);
+for (let index = 0; index < legearray.length; index++)
+{
+    console.log(legearray[index])
+}
+
