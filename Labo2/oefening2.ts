117c10afa51afceef221f3b64b2359726c406d48
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-02-26 23:38:55 +0100

    aanpassing oefening2 kleuren proberen reverse loop te fixen

--- Labo2/oefening2.ts
+++ Labo2/oefening2.ts
@@ -3,21 +3,13 @@ enum kleur
 Red, Orange, Yellow, Green, Blue, Indigo, Violet
 }
 let legearray = new Array(kleur);
-/*for (let index = 0; index < legearray.length; index++)
+for (let index = 0 ; index <= legearray.length; index++)
 {
-    legearray[index] 
+    legearray[index];
 }
-while(legearray.length <= 6)
-{
-    console.log(legearray);
-    break;
-}*/
+console.log(legearray)
 
-for (let index = 6; index > legearray.length ; index--)
-{
-    legearray[index] 
-}
-while(legearray.length)
+while(legearray.length <= 6)
 {
     console.log(legearray);
     break;
