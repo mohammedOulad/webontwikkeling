35a9f44b8ef9be8558f88c1b16e90a71a3d7767e
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-02-26 23:25:33 +0100

    oefening3 verbeterd van || naar && en de ternary functie juist gemaakt

--- Labo2/oefening3.ts
+++ Labo2/oefening3.ts
@@ -1,6 +1,6 @@
 let veelvouden = new Array();
 
-for (let i = 0; i < 70; i++)
+/*for (let i = -1; i <= 70; i++)
 {   
         veelvouden[i] = i+1;
         if (veelvouden[i] % 7 === 0)
@@ -8,18 +8,17 @@ for (let i = 0; i < 70; i++)
         console.log(veelvouden[i]);
     }
 }
-/*for (let i = 0; i < 70; i++)
+for (let i = 0; i < 70; i++)
 {   
         veelvouden[i] = i+1;
-        if ((veelvouden[i] % 3 === 0 || veelvouden[i] % 5 === 0) && !(veelvouden[i] % 3 === 0 || veelvouden[i] % 5 === 0))
+        if ((veelvouden[i] % 3 === 0 || veelvouden[i] % 5 === 0) && !(veelvouden[i] % 3 === 0 && veelvouden[i] % 5 === 0))
     {
         console.log(veelvouden[i]);
     }
 }*/
 for (let i = 0; i < 70; i++)
 {   
-    veelvouden[i] = (veelvouden[i] = i+1)
-    let result = veelvouden[i];
-    result =  ((veelvouden[i] % 3 === 0 || veelvouden[i] % 5 === 0) && !(veelvouden[i] % 3 === 0 || veelvouden[i] % 5 === 0)) 
-    console.log(result);
+    veelvouden[i] = (veelvouden[i] = i+1);
+    ((veelvouden[i] % 3 === 0 || veelvouden[i] % 5 === 0) && !(veelvouden[i] % 3 === 0 && veelvouden[i] % 5 === 0)) 
+      ? console.log(veelvouden[i]) : ""
 }
