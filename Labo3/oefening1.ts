dbca80f477c92349b9b5500779e947f95e5f2e3b
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-03-04 18:32:58 +0100

    kleine aanpassing voor index te tonen

--- Labo3/oefening1.ts
+++ Labo3/oefening1.ts
@@ -1,4 +1,5 @@
 let getallen = [2, 5, 3, 7, 8, 10, 15, 18, 24, 111, 12, 19, 87];
 console.log(getallen);
-var a = getallen.indexOf(24);
-console.log(a);
+console.log(getallen.indexOf(24));
+console.log(getallen.indexOf(5));
+console.log(getallen.indexOf(15));
