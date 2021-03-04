23829903a2e8dde4e0677f6fa034b97735f23ec6
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-03-04 19:28:44 +0100

    oefening2 for loop toevoegen

--- Labo3/oefening2.ts
+++ Labo3/oefening2.ts
@@ -1,6 +1,10 @@
 function reverseString  (a) {
     var reversedWord ="";
-
+    for (let i = a.length -1;i >= 0;i--)
+    {
+        reversedWord += a[i];
+    }
     return a;
     }
+    console.log(reverseString);
    
