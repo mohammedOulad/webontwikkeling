7d0d05ae1b8a8f9228e5a924604629ae43ca71a7
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-03-04 18:38:46 +0100

    naamgeving voor getallen let=>const

--- Labo3/oefening1.ts
+++ Labo3/oefening1.ts
@@ -1,4 +1,4 @@
-let getallen = [2, 5, 3, 7, 8, 10, 15, 18, 24, 111, 12, 19, 87];
+const getallen = [2, 5, 3, 7, 8, 10, 15, 18, 24, 111, 12, 19, 87];
 console.log(getallen);
 console.log(getallen.indexOf(24));
 console.log(getallen.indexOf(5));
