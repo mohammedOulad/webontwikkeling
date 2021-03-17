5dd93407b1847ebaf771ef1c33b3ec934cbd7f02
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-03-17 16:07:10 +0100

    volgens my de juiste versie van oefening2.ts

--- Labo5/oefening2.ts
+++ Labo5/oefening2.ts
@@ -1,12 +1,9 @@
-function sum(output:number):void {
-    console.log(output)
+function sum(output: number):void {
+    printToConsole (output);
 };
-let addoef2 = (a:number,b:number):void => {
-    sum(a+b) ;
-    };
-const printToConsole = (number : number) : void => 
+
+const printToConsole = (number : number) :void => 
 console.log(`the result is ${number}`); 
-addoef2(2,2);
-/*
-let c: number = addoef2(1,2);
-sum(c);*/
+sum(2+2+2);
+
+
