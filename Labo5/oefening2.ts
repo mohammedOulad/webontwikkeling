4bf01682b13338b5742854799fe3334a3a60bbb6
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-03-17 16:46:24 +0100

    oefening2 afgewerkt en aanmaken oefening 3

--- Labo5/oefening2.ts
+++ Labo5/oefening2.ts
@@ -2,7 +2,7 @@ interface ResultHandler {
     (number:number[])
 }
     
-const sum = (output: Array<number>,GroteDan10:ResultHandler) => {
+const sum = (output: Array<number>,GroteDan10:ResultHandler, KleinerDan10:ResultHandler) => {
     let optelling : number = 1;
     for (let i = 0;i < output.length; i++)
     optelling = optelling * output[i];
@@ -11,15 +11,16 @@ const sum = (output: Array<number>,GroteDan10:ResultHandler) => {
     {
         GroteDan10(output)
     }
-    else{
-        printToConsole (output);
+    else {
+        KleinerDan10 (output);
     }
 };
 const GroteDan10:ResultHandler = number => console.log(`the result ${number} is a very small number`)
-
+const KleinerDan10: ResultHandler = number => console.log(`the result is ${number}`); 
 const printToConsole = (number : Array<number>)  => 
 console.log(`the result is ${number}`); 
-sum([2+2+2],GroteDan10);
+
+sum([2+2+2],GroteDan10,KleinerDan10);
 
 
 
