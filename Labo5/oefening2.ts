7447e0722d8060d62ceaa48eda00a4a76495dd0a
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-03-17 16:22:05 +0100

    oefening2 array toegevoegd

--- Labo5/oefening2.ts
+++ Labo5/oefening2.ts
@@ -1,9 +1,22 @@
-function sum(output: number):void {
+/*interface ResultHandler {
+    (output:number):void
+}*/
+    
+const sum = (output: Array<number>) => {
+    let optelling : number = 1;
+    for (let i = 0;i < output.length; i++)
+    optelling = optelling * output[i];
     printToConsole (output);
 };
 
-const printToConsole = (number : number) :void => 
+
+const printToConsole = (number : Array<number>)  => 
 console.log(`the result is ${number}`); 
-sum(2+2+2);
+sum([2+2+2]);
+
+
+
+
+
 
 
