08611621493dfaf187132b7b9416b8e66762d403
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-03-17 16:33:45 +0100

    oefening2 afgewerkt

--- Labo5/oefening2.ts
+++ Labo5/oefening2.ts
@@ -1,19 +1,25 @@
-/*interface ResultHandler {
-    (output:number):void
-}*/
+interface ResultHandler {
+    (number:number[])
+}
     
-const sum = (output: Array<number>) => {
+const sum = (output: Array<number>,GroteDan10:ResultHandler) => {
     let optelling : number = 1;
     for (let i = 0;i < output.length; i++)
     optelling = optelling * output[i];
-    printToConsole (output);
-};
 
+    if(optelling >= 10)
+    {
+        GroteDan10(output)
+    }
+    else{
+        printToConsole (output);
+    }
+};
+const GroteDan10:ResultHandler = number => console.log(`the result ${number} is a very small number`)
 
 const printToConsole = (number : Array<number>)  => 
 console.log(`the result is ${number}`); 
-sum([2+2+2]);
-
+sum([2+2+2],GroteDan10);
 
 
 
