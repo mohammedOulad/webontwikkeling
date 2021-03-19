31a07b61cd33d524e246e157ba34dab9a748eb2d
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-03-19 22:29:20 +0100

    aanmaken functie calculateAnimalAge

--- Labo5/oefening4.ts
+++ Labo5/oefening4.ts
@@ -1,2 +1,4 @@
 const calculateDogAge = (DogAge:number):number =>DogAge*7;
 console.log(calculateDogAge(3));
+const calculateAnimalAge = (age:number, omzettingsverhouding:number ):number => omzettingsverhouding * age;
+console.log(calculateAnimalAge(2,7));
