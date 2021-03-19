fb193e81acb970fe314c5ef56d39ec214e62c01e
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-03-19 22:36:51 +0100

    afgewerkt oefening4

--- Labo5/oefening4.ts
+++ Labo5/oefening4.ts
@@ -1,6 +1,9 @@
 const calculateDogAge = (DogAge:number):number =>{return DogAge*7};
 console.log(calculateDogAge(3));
+
 const calculateAnimalAge = (age:number, omzettingsverhouding:number ):number => {return omzettingsverhouding * age};
 console.log(calculateAnimalAge(2,7));
 
-const calculateAnimalAgeFunctional
+const calculateAnimalAgeFunctional = (omzettingsverhouding:number) => (age) =>   {return calculateAnimalAge(age, omzettingsverhouding); }
+const calculateHamsterAge = calculateAnimalAgeFunctional(26);
+console.log(calculateHamsterAge(.5));
