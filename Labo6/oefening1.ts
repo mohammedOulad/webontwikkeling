2f05558c137ca6be1580e573a6f45a6e7e43e65a
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-03-25 23:59:35 +0100

    oefening1 evenMoreNumbers

--- Labo6/oefening1.ts
+++ Labo6/oefening1.ts
@@ -3,3 +3,4 @@ let numbers2: number[] = [4,5,6];
 let numbers3: number[] = [7,8,9];
 let allNumbers: number[] = [...numbers1,...numbers2,...numbers3]
 let extraNumbers: number[] = [...allNumbers,10,11,12]
+let evenMoreNumbers: number[] = [-2,-1,0,...extraNumbers,13,14,15]
