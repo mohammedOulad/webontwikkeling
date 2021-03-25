dc4901d7622a529feeb1f49871df31f8b3332767
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-03-26 00:25:49 +0100

    aanamken map

--- Labo6/oefening2.ts
+++ Labo6/oefening2.ts
@@ -1,5 +1,6 @@
 let names: string[] = ['joske','franske','donald','achmed']
 let capitalNames1: string[] = []
-let capitalNames2: string[] = []
+let capitalNames2: string[] = names.map(names2 =>names2.toUpperCase());
 names.forEach(names1=>capitalNames1.push(names1.toUpperCase()));
 console.log(capitalNames1);
+console.log(capitalNames2);
