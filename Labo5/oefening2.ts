9d8e112f8cae1f51d3e114778a6e55d30b08df5e
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-03-26 00:23:16 +0100

    aanamken foreach loop

--- Labo5/oefening2.ts
+++ Labo5/oefening2.ts
@@ -18,11 +18,11 @@ const sum = (output: Array<number>,Kleiner:ResultHandler, Groter:ResultHandler) 
 const KleinerDan10:ResultHandler = number => console.log(`the result ${number} is a very small number`)
 const GroterDan10: ResultHandler = number => console.log(`the result is ${number}`); 
 const printToConsole = (number : Array<number>)  => 
-console.log(`the result is ${number}`); 
+console.log(`the result is ${number}`);
 
 sum([2,2,2],KleinerDan10,GroterDan10);
 sum([2,2,6],KleinerDan10,GroterDan10);
-
+sum([2,2,2],GroterDan10,KleinerDan10);
 
 
 
---
+++ Labo6/oefening2.ts
@@ -0,0 +1,5 @@
+let names: string[] = ['joske','franske','donald','achmed']
+let capitalNames1: string[] = []
+let capitalNames2: string[] = []
+names.forEach(names1=>capitalNames1.push(names1.toUpperCase()));
+console.log(capitalNames1);
