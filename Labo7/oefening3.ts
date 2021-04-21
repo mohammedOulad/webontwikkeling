dc84e42df8138d968a324d3f34a965bd9bc6d3bb
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-04-21 17:34:47 +0200

    bijna afgewerkt probleem met for loop 12 moet .lenght van array zijn volgens my is het jokes.lenght

--- Labo7/oefening3.ts
+++ Labo7/oefening3.ts
@@ -2,15 +2,17 @@ const fetch = require("node-fetch");
 
 const showAllDadJokes = async () => {
   for (let i = 1; i < 3; i++) {
-    let jokes = await fetch(`https://icanhazdadjoke.com/search?term=dog&limit=5&page=${i}`,{
+    let jokes = await fetch(`https://icanhazdadjoke.com/search?term=dog&page=${i}`,{
         headers: {
           Accept: "application/json"
         }
       });
     let jokesJson = await jokes.json();
 
-    console.log(jokesJson);
-  }
-};
+   //console.log(jokesJson);
+      for (let i = 0; i < 12 ; i++){
+      console.log(jokesJson.results[i].joke)
+      }
+}};
 showAllDadJokes();
 export {};
