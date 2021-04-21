fb0b1d99ee455faf9ae9adcd84192d711170b13a
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-04-21 15:36:18 +0200

    response werkt niet helemaal

--- Labo7/oefening2.ts
+++ Labo7/oefening2.ts
@@ -1,11 +1,15 @@
 const fetch = require('node-fetch');
-let a:any = fetch(fetch('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11000')
-);
-let b:any = fetch(fetch('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11000')
-);
-let c:any = fetch(fetch('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11000')
-);
-Promise.all([a,b,c]).then((values: any) => {
-    console.log(values);
+
+let a:any = fetch('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11000').then((response:any) => response.json());
+
+let b:any = fetch('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11001').then((response:any) => response.json());
+
+let c:any = fetch('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11002').then((response:any) => response.json());
+
+Promise.all([a,b,c]).then((drinks: any) => {
+    console.log(drinks);
+    for (let drink of drinks) {
+        console.log(drinks.drink[0].strDrink);
+    }
 });
 export{};
