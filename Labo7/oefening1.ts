b6d8e21394f4b8bc034e5c5e5576301f2172ec1b
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-04-21 14:45:00 +0200

    oefening1 nogniet helemaal afgewerkt

--- Labo7/oefening1.ts
+++ Labo7/oefening1.ts
@@ -15,13 +15,33 @@ const slowMult = (a: number, b: number) => {
 }
 const slowDiv = (a: number, b: number) => {
     return new Promise<number>((resolve, reject) => {
+        
+        
+        if(a/b === 0)
+        {
         setTimeout(() => {
-            resolve(a*b);
+            resolve(a/b);
         },2000)
+    }
+        else{
+            reject("You cannot divide by zero")
+        }
     });
 }
 
 
+slowDiv(6,3).then((resultDiv:number )=> {
+    console.log(`6 / 3 = ${resultDiv}`)})
+    .catch(err =>{
+        console.error(err);
+    });
+    
+slowDiv(6,0).then((resultDiv:number )=> {
+        console.log(`6 / 0 = ${resultDiv}`)})
+        .catch(err =>{
+            console.error(err);
+        });
+    
 
 slowSum(1,5).then((result: number )=> {
     console.log(`1 + 5 = ${result}`)});
@@ -30,7 +50,6 @@ slowSum(1,5).then((result: number )=> {
         slowMult(result,2).then((resultMult: Number) =>{
             console.log(`1 + 5 = ${result} * 2 = ${resultMult}`)});
         });
-
 
 
 export{};
