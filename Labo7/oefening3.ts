bcb8f72fa1ee681ec4006787b3e2e969b5e0e9b8
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-04-22 00:23:03 +0200

    afwerking oefeing3.ts

--- Labo7/oefening3.ts
+++ Labo7/oefening3.ts
@@ -1,18 +1,34 @@
 const fetch = require("node-fetch");
 
 const showAllDadJokes = async () => {
-  for (let i = 1; i < 3; i++) {
-    let jokes = await fetch(`https://icanhazdadjoke.com/search?term=dog&page=${i}`,{
+  let aantalPaginas = await fetch(
+    `https://icanhazdadjoke.com/search?term=dog&limit=5&page=1`,
+    {
+      headers: {
+        'Accept': 'application/json'
+      }
+    }
+  )
+  
+  let aantalPaginasJson = await aantalPaginas.json();
+
+  for (let i = 1; i <= aantalPaginasJson.total_pages; i++) {
+    let jokes = await fetch(
+      `https://icanhazdadjoke.com/search?term=dog&limit=5&page=${i}`,
+      {
         headers: {
-          Accept: "application/json"
+          'Accept': 'application/json'
         }
-      });
+      }
+    )
     let jokesJson = await jokes.json();
 
-   //console.log(jokesJson);
-      for (let i = 0; i < 12 ; i++){
-      console.log(jokesJson.results[i].joke)
-      }
-}};
+    //console.log(jokesJson);
+    for (let x = 0; x < jokesJson.results.length ; x++) {
+      console.log(jokesJson.results[x].joke);
+      //console.log(jokesJson.results.length);
+    }
+  }
+};
 showAllDadJokes();
 export {};
