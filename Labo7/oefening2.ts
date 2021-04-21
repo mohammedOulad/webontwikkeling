466b1e1dfc199469dbe26738e39671d02c25ff70
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-04-21 15:08:17 +0200

    oefening2 site fetchen

--- Labo7/oefening2.ts
+++ Labo7/oefening2.ts
@@ -1,3 +1,11 @@
 const fetch = require('node-fetch');
-
-fetch('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11000')
+let a:any = fetch(fetch('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11000')
+);
+let b:any = fetch(fetch('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11000')
+);
+let c:any = fetch(fetch('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11000')
+);
+Promise.all([a,b,c]).then((values: any) => {
+    console.log(values);
+});
+export{};
