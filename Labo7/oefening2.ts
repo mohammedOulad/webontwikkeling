892ca671f7e30a1e97a0605e3790fb41bc6604e6
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-04-21 15:43:28 +0200

    oefening2 manueel optellen tellen

--- Labo7/oefening2.ts
+++ Labo7/oefening2.ts
@@ -10,5 +10,8 @@ Promise.all([a,b,c]).then((drinks: any) => {
     for (let drink of drinks) {
         console.log(drink.drinks[0].strDrink);
     }
+    console.log(drinks[0].drinks[0].strDrink);
+    console.log(drinks[1].drinks[0].strDrink);
+    console.log(drinks[2].drinks[0].strDrink);
 });
 export{};
