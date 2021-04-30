9295cb894fa9a9e0c6fd4777dee11e452ed7b9ef
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-04-30 20:06:03 +0200

    oefening2 afgewerkt met thisisme

--- Labo8/index.ts
+++ Labo8/index.ts
@@ -1,36 +1,38 @@
+import { profile} from "node:console";
+
 const express = require('express');
 const app = express();
+const ejs = require('ejs')
 app.set('port', 3000);
+app.set('view engine', 'ejs');
 
+const thisisme = {
+    name:'Mohammed Oulad mhand',
+    age: 22,
+    profilePic: 'https://www.nautiljon.com/images/description/00/04/1584134379085_image.jpg'
+};
 app.get('/',(req:any,res:any)=>{
     res.type('text/html');
-    res.send('Hello <strong>World</strong>')
+    res.send('zats absolutly crazy')
 });
-
 app.get('/whoami',(req:any,res:any)=>{
     res.type('text/html');
-    res.send('Hello <strong>World</strong>')
+    res.render('whoami',{name: thisisme.name, age: thisisme.age, profilePic:thisisme.profilePic})
 });
 app.get('/whoamijson',(req:any,res:any)=>{
-    res.type('text/html');
-    res.send('Hello <strong>World</strong>')
+res.json(thisisme);
 });
 app.get('/pikachujson',(req:any,res:any)=>{
     res.type('text/html');
-    res.send('Hello <strong>World</strong>')
+    res.send('Hello world>')
 });
 app.get('/pikachuhtml',(req:any,res:any)=>{
     res.type('text/html');
-    res.send('Hello <strong>World</strong>')
+    res.send('Hello world')
 });
+
 
 app.listen(app.get('port'), 
     ()=>console.log( '[server] http://localhost:' + app.get('port')));
 
-
-
-
-
-
-
-    
+export{};
