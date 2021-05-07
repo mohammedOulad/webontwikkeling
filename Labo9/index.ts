b831a6c0c7b5d2fcbdb4fe59f391220d45fe630a
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-05-07 23:49:07 +0200

    de style is een niet de beste daar word nog aan gewerkt en zal gedaan zijn voor zondag

--- Labo9/index.ts
+++ Labo9/index.ts
@@ -2,32 +2,44 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const fetch = require('node-fetch');
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended:true}))
 app.set('port', 3000);
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+
 let movies = [
     {name: "Classroom of the elite", myScore: 90},
     {name: "Solo Leveling", myScore: 100},
-    {name: "Beginning after the end", myScore: 95},
-    {name: "Darwin's game", myScore: 100}
+    {name: "Beginning after the end", myScore: 99},
+    {name: "Darwin's game", myScore: 85}
 ];
+app.get('/movies/:id',(req:any, res:any)=>{
+    let id = req.params.id;
+    console.log(id)
+    res.render("movie", {naam:movies[id].name, score:movies[id].myScore} );
+});
+
+app.get('/movies',(req:any,res:any)=>{  
+    res.render('movies',{movies: movies})
+});
+
 app.get('/',(req:any,res:any)=>{  
     res.type('text/html');
     res.render('landingpage')
 });
-app.get('/movies',(req:any,res:any)=>{  
-    res.render('movies',{animes:movies })
-});
-app.get('/movie',(req:any,res:any)=>{  
-    res.render('movie')
+
+app.get('/addMovie',(req:any,res:any)=>{  
+    res.render('addMovie')
 });
 
-app.get('/addmovie',(req:any, res:any)=>{
-    let title = req.params.title;
-    res.type('text/html')
-    res.render("animeS", {name:movies[title].name, score:movies[title].myScore, } );
-});
+app.post('/addMovie',(req:any, res:any)=>{
+    res.render('addMovie')
+    movies.push({name: req.body.title,myScore: req.body.score})
+})
+
+
 
 app.listen(app.get('port'), 
     ()=>console.log( '[server] http://localhost:' + app.get('port')));
